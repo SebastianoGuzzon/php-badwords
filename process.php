3d7f804dc04e4604479a4ead49d167b6a9d05130
Sebastiano <sebyguzz@icloud.com>
2023-05-17 01:44:19 +0200

    Commits su style, e cambio estensione

--- process.php
+++ process.php
@@ -1,18 +1,35 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  // recupera i dati inviati tramite POST
-  $paragraph = $_POST['paragraph'];
-  $censor = $_POST['censor'];
+<!DOCTYPE html>
+<html lang="en">
 
-  // stampa il paragrafo e la sua lunghezza
-  echo "Paragrafo: " . $paragraph;
-  echo "<br>Lunghezza: " . strlen($paragraph);
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Processo</title>
+</head>
 
-  // sostituisce tutte le occorrenze della parola da censurare con ***
-  $censored_paragraph = str_replace($censor, '***', $paragraph);
+<body>
 
-  // stampa il paragrafo censurato e la sua lunghezza
-  echo "<br>Paragrafo censurato: " . $censored_paragraph;
-  echo "<br>Lunghezza: " . strlen($censored_paragraph);
-}
-?>
+  <?php
+  if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // recupera i dati inviati tramite POST
+    $paragraph = $_POST['paragraph'];
+    $censor = $_POST['censor'];
+
+    // stampa il paragrafo e la sua lunghezza
+    echo "Paragrafo: " . $paragraph;
+    echo "<br>Lunghezza: " . strlen($paragraph);
+
+    // sostituisce tutte le occorrenze della parola da censurare con *
+    $censored_paragraph = str_replace($censor, '*', $paragraph);
+
+    // stampa il paragrafo censurato e la sua lunghezza
+    echo "<br>Paragrafo censurato: " . $censored_paragraph;
+    echo "<br>Lunghezza: " . strlen($censored_paragraph);
+  }
+  ?>
+
+
+</body>
+
+</html>
